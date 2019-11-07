0004f51ec901b40442a7efd222362a6465b0f4d2
Blake Mallory <mallorbc@mail.uc.edu>
2019-11-07 15:26:22 -0500

    started process of reading in data and splitting it and converting it to float

--- code/main.py
+++ code/main.py
@@ -2,9 +2,35 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 import cv2
+import re
 
-def load_images_text_file(path_to_file):
+def load_text_file(path_to_file):
+    all_data = []
+    float_data = []
     print("loaded")
+    with open(path_to_file) as f:
+        all_lines = f.read().splitlines()
+    for lines in all_lines:
+        lines = lines.split('\t')
+        # map(float, lines)
+        # # [float(i) for i in lines]
+        # # for entry in lines:
+        # #     lines = float(entry)
+        # print(type(lines[0]))
+        # print(len(lines))
+        # quit()
+
+    # file_to_read = open(path_to_file,"r")
+    # all_file_lines = file_to_read.readlines()
+    # for line in all_file_lines:
+    #     re.split(r"\t+",line)
+    #     print(len(line))
+    #     print(line)
+    #     quit()
+    #     # split_line = line.split(" ")
+    #     # print(len(split_line))
+    # quit()
+
 
 
 
@@ -21,5 +47,6 @@ if __name__ == '__main__':
     #gets the absolute path of the data and the labels
     data_location = os.path.abspath(data_location)
     labels_location = os.path.abspath(labels_location)
+    load_text_file(data_location)
     print(data_location)
     print(labels_location)
