202c13ca2fe4f99cf3fca9c2eeef856853035f50
Blake Mallory <blakecmallory@gmail.com>
2019-11-12 00:47:42 -0500

    got buggy mess of mlp done

--- code/layer.py
+++ code/layer.py
@@ -1,6 +1,60 @@
 from neuron import *
+import numpy as np
 
 
 class layer:
-    def __init__(self, number_of_neurons):
-        print("new layer with ", number_of_neurons, " neruons")
+    def __init__(self, number_of_inputs, number_of_outputs):
+        print("new layer with ", number_of_inputs,
+              " neruons and ", number_of_outputs, " outputs")
+        # sets the number of inputs and outputs
+        self.number_of_inputs = number_of_inputs
+        self.number_of_outputs = number_of_outputs
+        # sets the weights for the network
+        self.set_weights()
+        # sets the biases in the layer
+        self.set_biases()
+
+    def set_weights(self):
+        # print("set weights")
+        self.weights = np.random.normal(loc=0.0,
+                                        scale=np.sqrt(
+                                            2/(self.number_of_inputs+self.number_of_outputs)),
+                                        size=(self.number_of_inputs, self.number_of_outputs))
+        #print("test", len(self.weights[0]))
+
+    def set_biases(self):
+        self.biases = np.zeros(self.number_of_outputs)
+        # print(len(self.biases))
+        # print(self.biases)
+
+    def feed_forward(self, inputs):
+        return_values = np.dot(inputs, self.weights) + self.biases
+        return return_values
+
+    def feed_backward(self, inputs, gradient_output):
+        grad_input = np.dot(gradient_output, self.weights.T)
+
+        # compute gradient w.r.t. weights and biases
+        grad_weights = np.dot(inputs.T, gradient_output)
+        grad_biases = gradient_output.mean(axis=0)*inputs.shape[0]
+
+        #assert grad_weights.shape == self.weights.shape and grad_biases.shape == self.biases.shape
+
+        # Here we perform a stochastic gradient descent step.
+        self.weights = self.weights - self.learning_rate * grad_weights
+        self.biases = self.biases - self.learning_rate * grad_biases
+
+        return grad_input
+
+
+class sigmoid:
+    def __init__(self, inputs):
+        self.number_of_inputs = inputs
+
+    def feed_forward(self, inputs):
+        return_value = 1/(1+np.exp(-inputs))
+        return return_value
+
+    def feed_backward(self, inputs, gradient_output):
+        return_value = inputs * (1-inputs)
+        return gradient_output*return_value
--- code/mlp.py
+++ code/mlp.py
@@ -1,5 +1,7 @@
 from layer import *
 
+import math
+
 
 class Multi_Layer_Perceptron:
     def __init__(self, input_size, output_size, hidden_layers):
@@ -30,7 +32,84 @@ class Multi_Layer_Perceptron:
         self.make_layers()
 
     def make_layers(self):
-        for new_layer in self.neurons_in_each_layer:
-            print(new_layer)
-            self.network_layers.append(layer(new_layer))
+        for i in range(len(self.neurons_in_each_layer)-1):
+            # print(self.neurons_in_each_layer[i])
+
+            self.network_layers.append(
+                layer(self.neurons_in_each_layer[i], self.neurons_in_each_layer[i+1]))
+            self.network_layers.append(
+                sigmoid(self.neurons_in_each_layer[i+1]))
+        # print(self.network_layers)
         # print(self.network_layers[0].__dict__)
+
+    def feed_forward(self, inputs):
+        layer_outputs = []
+        for current_layer in self.network_layers:
+            layer_outputs.append(current_layer.feed_forward(inputs))
+            # grabs the last layer of outputs
+            inputs = layer_outputs[-1]
+
+        return layer_outputs
+
+    def make_prediction(self, inputs):
+        # print("will implement")
+        activations = self.feed_forward(inputs)
+        activations = activations[-1]
+        # print(len(activations))
+        # quit()
+        return activations
+
+    def adjust_prediction(self, predictions):
+        adjusted_predictions = []
+        print(np.size(predictions))
+        for predict in predictions:
+            print(len(predict))
+            quit()
+            if predict > 0.75:
+                adjusted_predictions.append(1)
+            elif predict < 0.25:
+                adjusted_predictions.append(0)
+            else:
+                adjusted_predictions.append(predict)
+        return adjusted_predictions
+
+    def calculate_loss(self, predictions, correct_labels):
+        correct_array = [0] * 10
+        correct_array[correct_labels] = 1
+        for i in range(len(predictions)):
+            loss = loss + math.pow(correct_array[i] - predictions[i], 2)
+        return loss/len(predictions)
+
+    def calculate_loss_gradient(self, loss):
+        return_value = loss * (1-loss)
+        return return_value
+
+    def train(self, inputs, correct_class):
+        # make predictions
+        predictions = self.make_prediction(inputs)
+        predictions = self.adjust_prediction(predictions)
+        # get loss
+        loss = calculate_loss(predictions, correct_class)
+        print(loss)
+        loss_gradient = calculate_loss_gradient(loss)
+
+        for current_layer in self.network_layers:
+            loss_gradient = current_layer.feed_backward(
+                current_layer.weights, loss_gradient)
+
+    # def calculate_loss(self, predictions, correct_labels):
+    #     correc_array = []
+    #     for i in range(len(predictions)):
+
+    #     squared_errors = predictions
+    # for i in range(len(predictions)):
+    #     for j in range(len(predictions[i])):
+    #         if correct_labels[i] == j:
+    #             correct_value = 1
+    #         else:
+    #             correct_value = 0
+    #         loss = loss + math.pow(predictions[i][j]-correct_value, 2)
+    # loss = loss/2
+    # return loss
+
+    # def check_predictions(labels):
