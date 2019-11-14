3d2c58d420a4e912aca29fbf5f8be08ca817c05c
Blake Mallory <blakecmallory@gmail.com>
2019-11-14 18:46:32 -0500

    added more scripts; added document images

---
+++ code/make_bars.sh
@@ -0,0 +1,1 @@
+python main.py -m 10 -d ../hw_files/output/train_images.txt -td ../hw_files/output/test_images.txt -ld reloaded_encoder/11-14-16-12-11/checkpoints/checkpoint2580/ -l ../hw_files/output/train_labels.txt  -tl ../hw_files/output/test_labels.txt
--- code/reconstruct_encoder.sh
+++ code/reconstruct_encoder.sh
@@ -1,1 +1,1 @@
-python main.py -m 8 -td ../hw_files/output/test_images.txt -tl ../hw_files/output/test_labels.txt -ld auto_out/11-14-09-53-02/checkpoints/checkpoint4200/
+python main.py -m 8 -td ../hw_files/output/test_images.txt -tl ../hw_files/output/test_labels.txt -ld reloaded_encoder/11-14-16-12-11/checkpoints/checkpoint3080/
